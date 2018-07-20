a23dda64f193a885b876599c3c9d90b981aea5f5
jiraphon <jiraphon@homedottech.com>
2018-07-20 14:28:45 +0700

    create modal

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
-import placeImage from './src/assets/cat_noun_001_02368.jpg';
+// import placeImage from './src/assets/cat_noun_001_02368.jpg';
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
 
 export default class App extends React.Component {
   state = {
-    places: []
+    places: [],
+    selectedPlace: null
   }
 
   placeAddedHandler = placeName => {
@@ -25,13 +27,40 @@ export default class App extends React.Component {
     })
   }
 
-  placeDeletedHandler = key => {
+  // placeDeletedHandler = key => {
+  //   this.setState(prevState => {
+  //     return {
+  //       places: prevState.places.filter(place => {
+  //         return place.key !== key 
+  //       })
+  //     }
+  //   })
+  // }
+
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key 
+        })
+      }
+    })
+  }
+
+  placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
         places: prevState.places.filter(place => {
-          return place.key !== key 
-        })
+          return place.key !== prevState.selectedPlace.key 
+        }),
+        selectedPlace: null
       }
+    })
+  }
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
     })
   }
 
@@ -39,10 +68,14 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <PlaceDetail 
+          selectedPlace={this.state.selectedPlace} 
+          onItemDeleted={this.placeDeletedHandler} 
+          onModalClosed={this.modalClosedHandler} />
         <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
         <PlaceList 
           places={this.state.places} 
-          onItemDeleted={this.placeDeletedHandler}/>
+          onItemSelected={this.placeSelectedHandler}/>
       </View>
     );
   }
